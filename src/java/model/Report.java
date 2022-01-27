f9146c829fb3b849a91b3042f91fc603af98e364
NostrumPyre <watchdogs13.xv@gmail.com>
2022-01-27 11:01:06 +0800

    Add function into therapist model

--- src/java/model/Report.java
+++ src/java/model/Report.java
@@ -5,6 +5,11 @@
  */
 package model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.Date;
+import java.sql.ResultSet;
+
 /**
  *
  * @author rhyth
@@ -14,12 +19,12 @@ public class Report {
     private int reportid;
     private int appointmentid;
     private String result;
-    private String date;
+    private Date date;
 
     public Report() {
     }
 
-    public Report(int reportid, int appointmentid, String result, String date) {
+    public Report(int reportid, int appointmentid, String result, Date date) {
         this.reportid = reportid;
         this.appointmentid = appointmentid;
         this.result = result;
@@ -50,15 +55,55 @@ public class Report {
         this.result = result;
     }
 
-    public String getDate() {
+    public Date getDate() {
         return date;
     }
 
-    public void setDate(String date) {
+    public void setDate(Date date) {
         this.date = date;
     }
     
     
+    public void addReport(int appointment_id,String result,Date date) {
+    Connection conn;
+    PreparedStatement ps;
+      try{
+          String SQL = "INSERT INTO REPORT(appointment_id, result,date)VALUES(?,?,?)";
+          conn = DBConnection.openConnection();
+          ps = conn.prepareStatement(SQL);
+          ps.setInt(1, appointment_id);
+          ps.setString(2, result);
+          ps.setDate(3, date);
+          ps.executeUpdate();
+          
+      }catch(Exception e){}  
+    }
     
+    public Report getReport(int id) {
+      Connection conn;
+      PreparedStatement ps;
+      ResultSet rs;   
+      Report r = new Report();
+      
+      try{
+          String SQL = "SELECT * FROM REPORT WHERE id=?";
+          conn = DBConnection.openConnection();
+          ps = conn.prepareStatement(SQL);
+          ps.setInt(1, id);
+          rs = ps.executeQuery();
+       
+          while(rs.next())
+          {
+              r.setReportid(rs.getInt("id"));
+              r.setAppointmentid(rs.getInt("appointment_id"));
+              r.setResult(rs.getString("result"));
+              r.setDate(rs.getDate("date"));
+          }
+          
+      }catch(Exception e){}
+      
+      return r;
+        
+    }
     
 }
